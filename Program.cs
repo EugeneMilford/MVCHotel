0a004f5c72e31149c835f94d286a3896160a8400
Eugene Milford <gmilford321@gmail.com>
2024-01-12 18:02:12 +0200

    HTML Added

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Welcome}/{action=Index}/{id?}");
 
 app.Run();
