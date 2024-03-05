7e2559d21c1167b6fbc6789a1a03c6bcf42b2c20
Eugene Milford <gmilford321@gmail.com>
2024-03-05 15:56:30 +0200

    Outdoors Controller Added

---
+++ Controllers/OutdoorsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HotelManagement.Data;
+using HotelManagement.Models;
+
+namespace HotelManagement.Controllers
+{
+    public class OutdoorsController : Controller
+    {
+        private readonly HotelContext _context;
+
+        public OutdoorsController(HotelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Activity
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Activities.ToListAsync());
+        }
+
+        // GET: Activity/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Activities == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activities
+                .FirstOrDefaultAsync(m => m.ActivityID == id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        // GET: Activity/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Activity/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ActivityID,GuestName,NumberOfPeople,BookingTime,ActivityName,Confirmed")] Activities activity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(activity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(activity);
+        }
+
+        // GET: Activity/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Activities == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activities.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+            return View(activity);
+        }
+
+        // POST: Activity/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ActivityID,GuestName,NumberOfPeople,BookingTime,ActivityName,Confirmed")] Activities activity)
+        {
+            if (id != activity.ActivityID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(activity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActivityExists(activity.ActivityID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(activity);
+        }
+
+        // GET: Activity/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Activities == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _context.Activities
+                .FirstOrDefaultAsync(m => m.ActivityID == id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return View(activity);
+        }
+
+        // POST: Activity/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Activities == null)
+            {
+                return Problem("Entity set 'HotelContext.Activity'  is null.");
+            }
+
+            var activity = await _context.Activities.FindAsync(id);
+            if (activity != null)
+            {
+                _context.Activities.Remove(activity);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActivityExists(int id)
+        {
+            return _context.Activities.Any(e => e.ActivityID == id);
+        }
+    }
+}
+
